95ddc88fe4e90ac8dee5dafe62a1bb60795e90fd
alxjrvs@gmail.com <alxjrvs@gmail.com>
2023-07-16 06:54:30 -0400

    Good enough to move to view components. Maybe.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,7 +5,12 @@ class Contact < ApplicationRecord
   include BroadcastUpdateable
 
   belongs_to :player_character
+  belongs_to :crew
   belongs_to_static :playbook
+
+  def source
+    player_character || crew
+  end
 
   default_scope { order(:order) }
 end
